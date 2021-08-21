46c590e277cd60e6091cabb550179b923a254f28
Manjunath0408 <nath.vasam@gmail.com>
2021-08-21 14:50:51 +0530

    activites added

---
+++ app/src/main/java/com/example/connectforacause/OrganisationListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.connectforacause
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrganisationListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_organisation_list)
+    }
+}
